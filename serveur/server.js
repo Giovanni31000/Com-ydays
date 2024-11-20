ffdc3e6b246df65323f1965d46e2eb2fe363840d
dgiovann <giovanni.damiens@ynov.com>
2024-11-20 11:35:27 +0100

    index and event

--- serveur/server.js
+++ serveur/server.js
@@ -16,6 +16,36 @@ const server = http.createServer((req, res) => {
                 res.end(data);
             }
         });
+    } else if (req.url === '/style/style.css') {
+        fs.readFile(path.join(__dirname, '../style/style.css'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('500 - Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/css' });
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/style/EventStyle.css') {
+        fs.readFile(path.join(__dirname, '../style/EventStyle.css'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('500 - Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/css' });
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/event.html') {
+        fs.readFile(path.join(__dirname, '../web/event.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('500 - Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('404 - Not Found');
